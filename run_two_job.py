42d8f2ea1233d0dba8f8cbb9b54198255d10550c
Lai-JX <2840026059@qq.com>
2024-04-01 16:14:11 +0800

    add batchsize config

--- run_two_job.py
+++ run_two_job.py
@@ -5,11 +5,15 @@ import subprocess
 from multiprocessing import Process
 import time
 
-def run_job():
-    command_to_run = 'docker exec -it muri /bin/bash -c "/root/nfs-share/mps_test/run_two_job.sh -g {} -f {}  -s {} -p {}"'.format(num_gpu, job[0], job[1], path1)
+def run_job(num_gpu, job0, bs0, job1, bs1, path):
+    if job1 is None:
+        command_to_run = '/workspace/nfs-share/mps_test/run_two_job.sh -g {} -f {} -x {} -p {}'.format(num_gpu, job0, bs0, path)  
+    else:
+        command_to_run = '/workspace/nfs-share/mps_test/run_two_job.sh -g {} -f {} -x {} -s {}  -y {} -p {}'.format(num_gpu, job0, bs0, job1, bs1, path)  
     print('run:',command_to_run)    
     output = run_command(command_to_run)
     print("run_job_exit\n")
+    print("------------------------------------------------------------------------------")
 
 def dcgmi(path):
     print(path)
@@ -17,7 +21,7 @@ def dcgmi(path):
     try:
         # 执行命令并等待返回结果
         result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
-        print("dcgmi_exit\n")
+        print("dcgmi_exit")
         return result.stdout
     except Exception as e:
         # 如果命令执行失败，捕获异常并处理
@@ -43,33 +47,42 @@ def run_command(command):
 if __name__ == "__main__":
     jobs = ['resnet_18','shufflenet_v2_x1_0','vgg19','vgg16','dqn','a2c','bert','gpt2']
     combinations_lst = list(itertools.combinations(jobs, 2))
-    combinations_lst = [('resnet_18', 'vgg19'), ('shufflenet_v2_x1_0', 'vgg19'), ('shufflenet_v2_x1_0', 'gpt2')]
-    combinations_lst = [('vgg19','vgg16'),('resnet_18', 'vgg19')]
-    combinations_lst = [('bert','gpt2')]
-    combinations_lst = [('vgg16','dqn'),('bert','gpt2'),('dqn','a2c')]
-    combinations_lst = [('vgg19','gpt2')]
+    # combinations_lst = [('resnet_18', 'vgg19'), ('shufflenet_v2_x1_0', 'vgg19'), ('shufflenet_v2_x1_0', 'gpt2')]
+    # combinations_lst = [('vgg19','vgg16'),('resnet_18', 'vgg19')]
+    # combinations_lst = [('bert','gpt2')]
+    # combinations_lst = [('vgg16','dqn'),('bert','gpt2'),('dqn','a2c')]
+    # combinations_lst = [('vgg19','gpt2')]
+    combinations_lst = ['shufflenet_v2_x1_0','vgg19','vgg16','dqn','a2c','bert','gpt2']
     print(combinations_lst)
     count = 0
-    # for num_gpu in [1,2,4,8]:
-    for num_gpu in [8]:
+    for num_gpu in [1,2,4,8]:
+    # for num_gpu in [8]:
         
-        dir1 = '/home/jxlai/nfs-share/mps_test/IterTime/two_job1_mps/gpu{}/'.format(num_gpu)
-        dir2 = '/home/jxlai/nfs-share/mps_test/result/two_job1_mps/gpu{}/'.format(num_gpu)
-        os.system('mkdir -p {}; mkdir -p {};'.format(dir1, dir2))
+        dir1_1 = '/workspace/nfs-share/mps_test/IterTime/single/gpu{}/'.format(num_gpu)
+        dir2_1 = '/workspace/nfs-share/mps_test/result/single/gpu{}/'.format(num_gpu)
+        os.system('mkdir -p {}; mkdir -p {};'.format(dir1_1, dir2_1))
         for job in combinations_lst:
-            # 启动job
-            path1 = (dir1+job[0]+'--'+job[1]).replace('/home/jxlai', '/root')
-            p1 = Process(target=run_job)
-            p1.start()
+            if isinstance(job, str):  
+                dir1_2, dir2_2 = dir1_1+job+'/', dir2_1+job+'/'
+            else:
+                dir1_2, dir2_2 = dir1_1+job[0]+'--'+job[1]+'/', dir2_1+job[0]+'--'+job[1]+'/'
+            os.system('mkdir -p {}; mkdir -p {};'.format(dir1_2, dir2_2))
+            for bs in [32, 128, 512, 2048]:
+                # 启动job
+                if isinstance(job, str):                                            # 单一job 
+                    p1 = Process(target=run_job, args=[num_gpu, job, bs, None, None, dir1_2+str(bs)])
+                else:                                                               # 2 jobs
+                    p1 = Process(target=run_job, args=[num_gpu, job[0], bs, job[1], bs, dir1_2+str(bs)])
+                p1.start()
 
-            time.sleep(10)
-            # 收集指标数据
-            p2 = Process(target=dcgmi, args=[dir2+job[0]+'--'+job[1]])
-            p2.start()
+                time.sleep(10)
+                # 收集指标数据
+                p2 = Process(target=dcgmi, args=[dir2_2+str(bs)])
+                p2.start()
 
-            p1.join()
-            p2.join()
-            count += 1
+                p1.join()
+                p2.join()
+                count += 1
         if num_gpu==1:
             run_command('dcgmi group -g 2 -a 1')
         if num_gpu==2:
--- run_two_job.sh
+++ run_two_job.sh
@@ -1,17 +1,40 @@
 #! /bin/bash
-
-cmd=" --allow-run-as-root --oversubscribe --prefix /root/share/openmpi -bind-to none -map-by slot -mca btl_tcp_if_include 10.249.46.12/21  -x LD_LIBRARY_PATH -x PATH  -mca pml ob1 -mca btl ^openib /root/anaconda3/envs/muri/bin/python3 /root/nfs-share/mps_test/main_util_single.py "
-
-resnet_18="--model0 resnet18 --batch-size0 128 --train-dir0 /root/datasets/ILSVRC2012/train/ --num-workers0 0 --prefetch-factor0 2 --iters0 5280 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-shufflenet_v2_x1_0="--model0 shufflenet_v2_x1_0 --batch-size0 128 --train-dir0 /root/datasets/ILSVRC2012/train/ --num-workers0 0 --prefetch-factor0 2 --iters0 5020 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-vgg19="--model0 vgg19 --batch-size0 16 --train-dir0 /root/datasets/ILSVRC2012/train/ --num-workers0 0 --prefetch-factor0 2 --iters0 2000 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-vgg16="--model0 vgg16 --batch-size0 16 --train-dir0 /root/datasets/ILSVRC2012/train/ --num-workers0 0 --prefetch-factor0 2 --iters0 4000 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-dqn="--model0 dqn --batch-size0 128 --train-dir0 ./ --num-workers0 0 --prefetch-factor0 2 --iters0 5000 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-a2c="--model0 a2c --batch-size0 64 --train-dir0 ./ --num-workers0 0 --prefetch-factor0 2 --iters0 1200 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-bert="--model0 bert --batch-size0 4 --train-dir0 /root/nfs-share/Muri_exp/workloads/wikitext-2-raw/wiki.train.raw --num-workers0 0 --prefetch-factor0 2 --iters0 3000 --job-id0 0  --this-dir /root/nfs-share/mps_test"
-gpt2="--model0 gpt2 --batch-size0 4 --train-dir0 /root/nfs-share/Muri_exp/workloads/wikitext-2-raw/wiki.train.raw --num-workers0 0 --prefetch-factor0 2 --iters0 3000 --job-id0 0  --this-dir /root/nfs-share/mps_test"
+# -mca btl_tcp_if_include 10.249.46.12/21 
+cmd=" --allow-run-as-root --oversubscribe --prefix /root/share/openmpi -bind-to none -map-by slot -x LD_LIBRARY_PATH -x PATH  -mca pml ob1 -mca btl ^openib /root/anaconda3/envs/muri/bin/python3 /workspace/nfs-share/mps_test/main_util_single.py "
+job=""
+get_job()
+{
+   # args: job_name; batch_size
+   resnet_18="--model0 resnet18 --batch-size0 128 --train-dir0 /share/datasets/ILSVRC2012_img_train/ --num-workers0 0 --prefetch-factor0 2 --iters0 5280 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   shufflenet_v2_x1_0="--model0 shufflenet_v2_x1_0 --batch-size0 128 --train-dir0 /share/datasets/ILSVRC2012_img_train/ --num-workers0 0 --prefetch-factor0 2 --iters0 5020 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   vgg19="--model0 vgg19 --batch-size0 16 --train-dir0 /share/datasets/ILSVRC2012_img_train/ --num-workers0 0 --prefetch-factor0 2 --iters0 2000 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   vgg16="--model0 vgg16 --batch-size0 16 --train-dir0 /share/datasets/ILSVRC2012_img_train/ --num-workers0 0 --prefetch-factor0 2 --iters0 4000 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   dqn="--model0 dqn --batch-size0 128 --train-dir0 ./ --num-workers0 0 --prefetch-factor0 2 --iters0 5000 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   a2c="--model0 a2c --batch-size0 64 --train-dir0 ./ --num-workers0 0 --prefetch-factor0 2 --iters0 1200 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   bert="--model0 bert --batch-size0 4 --train-dir0 /workspace/nfs-share/Muri_exp/workloads/wikitext-2-raw/wiki.train.raw --num-workers0 0 --prefetch-factor0 2 --iters0 3000 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   gpt2="--model0 gpt2 --batch-size0 4 --train-dir0 /workspace/nfs-share/Muri_exp/workloads/wikitext-2-raw/wiki.train.raw --num-workers0 0 --prefetch-factor0 2 --iters0 3000 --job-id0 0  --this-dir /workspace/nfs-share/mps_test"
+   job_raw=`eval echo '$'"$1"`
+   i=0
+   job=""
+   if [ ! -n "$2" ]; then
+      echo "use default batchsize"
+      job=$job_raw
+   else
+      for var in ${job_raw[@]}
+      do
+         ((i++))
+         if [ $i -eq 4 ]; then
+            job=$job$2" "
+         else
+            job=$job$var" "
+         fi
+      done
+   fi
+   
+}
 kill_all()
 {
+   # args:command name
     # ID=`ps -ef | grep "$1" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`  # -v表示反过滤，awk表示按空格或tab键拆分，{print $2}表示打印第二个（这里对应进程号）
     ID=`ps | grep "$1" | grep -v "$0" | grep -v "grep" | awk '{print $1}'`  # -v表示反过滤，awk表示按空格或tab键拆分，{print $2}表示打印第二个（这里对应进程号）
     for id in $ID 
@@ -22,14 +45,18 @@ kill_all()
     done
 }
 
+# echo $array
+
 # 1. 获取参数
 num_gpu=1
 job1_str=''
 job2_str=''
+job1_bs=''
+job2_bs=''
 job1=''
 job2=''
 path=''
-while getopts "g:f:s:p:h" arg #选项后面的冒号表示该选项需要参数
+while getopts "g:f:s:p:x:y:h" arg #选项后面的冒号表示该选项需要参数
 do
         case $arg in
              g)
@@ -37,13 +64,19 @@ do
                 ;;
              f)
                 job1_str=$OPTARG
-                job1=`eval echo '$'"$job1_str"`
+               #  job1=`eval echo '$'"$job1_str"`
                 # echo $job1
                 ;;
              s)
                 job2_str=$OPTARG
-                job2=`eval echo '$'"$job2_str"`
+               #  job2=`eval echo '$'"$job2_str"`
                 # echo $job2
+                ;;
+             x)
+                job1_bs=$OPTARG
+                ;;
+             y)
+                job2_bs=$OPTARG
                 ;;
              p)
                 path=$OPTARG
@@ -53,6 +86,8 @@ do
                 echo "-g                The number of gpu."
                 echo "-f                First job"
                 echo "-s                Second job"
+                echo "-x                First job batchsize"
+                echo "-y                Second job batchsize"
                 echo "-p                IterTime output path"
                 exit
                 ;;
@@ -64,14 +99,25 @@ do
         esac
 done
 
-cmd1=$num_gpu$cmd$job1
-cmd2=$num_gpu$cmd$job2
-# echo $cmd1
-# echo $cmd2
+get_job $job1_str $job1_bs
+job1=$job
+exit 0
+echo $job2_str
+if [ "$job2_str" == "" ]; then                                       # one job
+   cmd1=$num_gpu$cmd$job1
+   echo $cmd1
+   /root/share/openmpi/bin/mpirun -n $cmd1 > $path-$job1_str &
+else                                                                 # two job
+   get_job $job2_str $job2_bs
+   job1=$job
+   cmd1=$num_gpu$cmd$job1
+   cmd2=$num_gpu$cmd$job2
+   echo $cmd1
+   echo $cmd2
 
-/root/share/openmpi/bin/mpirun -n $cmd1 > $path-$job1_str &
-/root/share/openmpi/bin/mpirun -n $cmd2 > $path-$job2_str &
-
+   /root/share/openmpi/bin/mpirun -n $cmd1 > $path-$job1_str &
+   /root/share/openmpi/bin/mpirun -n $cmd2 > $path-$job2_str &
+fi
 sleep 155s
-kill_all "python"
+kill_all "mpirun"
 exit 0
